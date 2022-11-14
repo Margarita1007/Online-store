64a6c09d2a3b560b58054810d61a2f8336c15898
Margarita Bakulina <96014012+Margarita1007@users.noreply.github.com>
2022-11-14 17:25:21 +0400

    Add files via upload

---
+++ build/precache-manifest.c915acd2e75b4231cfbdb7f32bc6abfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b41805508a1fa4c90e8",
+    "url": "/static/css/main.8f12aba3.chunk.css"
+  },
+  {
+    "revision": "8b41805508a1fa4c90e8",
+    "url": "/static/js/main.dcdc140d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3c287d9b22c8af750432",
+    "url": "/static/js/2.3dc3ac60.chunk.js"
+  },
+  {
+    "revision": "8d97220782618666469e9afac3710982",
+    "url": "/static/media/logo2.8d972207.png"
+  },
+  {
+    "revision": "71b0e934a6aa4189d0748f6a08058bd4",
+    "url": "/static/media/blackloupe.71b0e934.svg"
+  },
+  {
+    "revision": "86f2cae513896026f7cc052c53d0ff5d",
+    "url": "/index.html"
+  }
+];
