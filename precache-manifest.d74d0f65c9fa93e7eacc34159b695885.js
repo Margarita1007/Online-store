41be6fc768b32af841ee1b55782a14a25db140f4
Margarita1007 <m.kenshova@mail.ru>
2022-11-15 20:11:41 +0400

    Updates

---
+++ precache-manifest.d74d0f65c9fa93e7eacc34159b695885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ff4efa7aae8e3de507d",
+    "url": "/Online-store/static/css/main.6ec0d99a.chunk.css"
+  },
+  {
+    "revision": "3ff4efa7aae8e3de507d",
+    "url": "/Online-store/static/js/main.a6c9035e.chunk.js"
+  },
+  {
+    "revision": "d652c3d6631c01b7c867",
+    "url": "/Online-store/static/js/runtime~main.005a2437.js"
+  },
+  {
+    "revision": "3c287d9b22c8af750432",
+    "url": "/Online-store/static/js/2.3dc3ac60.chunk.js"
+  },
+  {
+    "revision": "8d97220782618666469e9afac3710982",
+    "url": "/Online-store/static/media/logo2.8d972207.png"
+  },
+  {
+    "revision": "675687c0dedad1f74c036dd0f14e292a",
+    "url": "/Online-store/static/media/blackloupe.675687c0.svg"
+  },
+  {
+    "revision": "edce92059df31265d486bfc4b9f309ad",
+    "url": "/Online-store/index.html"
+  }
+];
